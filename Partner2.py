a21e800375a337bdf3f3e800c675e1e9331620d4
alvarezandres27-lab <alvarez.andres3000@gmail.com>
2026-02-16 18:07:12 +0000

    This is my change
    Co-authored-by: AlyssaCastellano <AlyssaCastellano@users.noreply.github.com>

---
+++ Partner2.py
@@ -0,0 +1,20 @@
+''''''
+
+#Alyssa Castellano Partner 1
+#Andres Alvarez Partner 2
+
+#This working
+#Andres: How are you doing
+#Alyssa: I'm doing well, what about you?
+#Andres: I think I'm gonna turn into a vampire.
+#Alyssa: I think i'm gonna turn into a werewolf >:D
+''''''
+
+#Excersizes
+print("Hello World")
+
+print("Mmm bloodddddd!!")
+
+print("Nah I'm a walmart bag")
+print("hi")
+print("AHHHHHH BAGSSSSS JWNDJINWJNWK!!!")
